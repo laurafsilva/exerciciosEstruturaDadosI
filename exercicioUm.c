038f13f9b0f0e6f9bc394c811bb644f2e8c474f2
Laura Silva <144743148+laurafsilva@users.noreply.github.com>
2025-08-27 09:09:07 -0300

    Update exercicioUm.c

--- exercicioUm.c
+++ exercicioUm.c
@@ -1,3 +1,14 @@
+/*Questão 1:
+Suponha que:
+a = 3
+b = a / 2
+c = b + 3.1
+Qual é o valor de c?
+q c = 4.6
+q c = 4.1
+q c = 4
+q Nenhuma das opções acima
+q Não é possível determinar o valor de c*/
 #include<stdio.h>
 int main(){
     int a=3, b=0;
