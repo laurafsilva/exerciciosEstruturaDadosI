16ee43d87a5270b73fbce1ca1d7ce793d02f6a17
Laura Silva <144743148+laurafsilva@users.noreply.github.com>
2025-08-27 09:07:15 -0300

    Update exercicioDois.c

--- exercicioDois.c
+++ exercicioDois.c
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+int calcularMedia(int notaUm, int notaDois, int notaTres);
+int main(){
+    int G1, G2, G3;
+    scanf("%d", &G1);
+    scanf("%d", &G2);
+    scanf("%d", &G3);
+    if((G1<=3 || G2<=3 || G3<=3)|| calcularMedia(G1, G2, G3)<5){
+        printf("REPROVADO");
+    }
+    else{
+        printf("APROVADO");
+    }
+    return 0;
+}
+int calcularMedia(int notaUm, int notaDois, int notaTres){
+    int media = 0;
+    media = (notaUm + notaDois + notaTres)/3;
+    return media;
+}
